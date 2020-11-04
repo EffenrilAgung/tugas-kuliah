ed7220160f06bdd691bba08ad52d4b4d04bbe429
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2020-11-04 10:50:14 +0700

    Add files via upload

---
+++ Luas-keliling-persegi-panjang.py
@@ -0,0 +1,25 @@
+print("=====Nama Kelompok=====")
+print("Effenril Agung")
+print("Niki Pranata")
+print("Muhammad Risky")
+print("Muhammad Ridwan")
+print("")
+# Diketahui
+p = int(input('Masukkan Panjang Persegi Panjang = '))
+l = int(input('Masukkan Luas Persegi Panjang = '))
+print("Panjang = ", p)
+print("Lebar = ",l)
+print("")
+
+# Rumus Mencari Luas p x l
+luas = p * l
+print("luas = ", p,"cm", "x", l,"cm")
+print("Luas = ", luas)
+print("")
+#  Rumus Mencari Keliling Persegi Panjang Adalah 2 x (p + l)
+keliling = 2 * (p + l)
+print("Keliling = 2 * (",p,"cm", "+", l,"cm )")
+print("Keliling =  2 x", p + l)
+print("Keliling = ",keliling,"cm")
+print("")
+print("Jadi, hasil dari Luas Persegi panjang =", luas, "cm2", "dan keliling persegi panjang adalah =" , keliling,"CM")
